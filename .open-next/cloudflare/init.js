982e20ef3daffe7623120f3071c2c3e0c6dea97c
Raphite White <raphitewhite@gmail.com>
2026-03-08 14:02:18 +0700

    m

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1772905291070,
+    __BUILD_TIMESTAMP_MS__: 1772952968885,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
